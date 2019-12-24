4bf6ad2955abf3c73f45b12574d06c8b25eb409a
Agnideep Ghosh <41577937+agnideepghosh@users.noreply.github.com>
2019-12-24 18:57:03 +0530

    Add files via upload

---
+++ LeaveApplicationCode.py
@@ -0,0 +1,14 @@
+#to help you all to make your leave applications
+#github.com/agnideepghosh
+
+x=input("Enter your name:")
+y=input("Enter the day you took leave:")
+z=input("Enter the cause of leave in 1 word or more:")
+yourclass=int(input("You are in which grade?"))
+
+print("Respected Sir/Madam")
+print("My name is", x,"and I am in", yourclass,"grade")
+print("This is to inform you that i was on leave on", y,"as I had", z)
+print("This caused me to not attend the classes.")
+print("Yours sincerely")
+print(x)
